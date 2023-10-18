54c875eef4530194efccacf419782fd21600ae5e
Noah <noahsu@chalmers.se>
2023-10-18 13:59:12 +0200

    Keep on walking

---
+++ server.erl
@@ -0,0 +1,25 @@
+-module(server).
+-export([start/1,stop/1]).
+
+% Start a new server process with the given name
+% Do not change the signature of this function.
+
+start(ServerAtom) ->
+    gen_server:start(ServerAtom,[],fun mgt/2).
+    % TODO Implement function
+    % - Spawn a new process which waits for a message, handles it, then loops infinitely
+    % - Register this process to ServerAtom
+    % - Return the process ID
+    not_implemented.
+mgt(S,{join, Ch, Client}) ->
+    
+loop() ->
+    do:something(),
+    loop().
+
+% Stop the server process registered to the given name,
+% together with any other associated processes
+stop(ServerAtom) ->
+    % TODO Implement function
+    % Return ok
+    not_implemented.
